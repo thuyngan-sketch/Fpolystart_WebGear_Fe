c8f1812fe117e2dc239eed6a9fdc7d14c6f1cb43
thuyngan-sketch <nt975981@gmail.com>
2024-11-16 12:22:13 +0700

    update order

---
+++ src/app/service/order.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable, throwError } from 'rxjs'
+import { catchError } from 'rxjs/operators'
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+  private apiUrl = 'https://localhost:7249/api/Orders'
+  constructor(private http: HttpClient) { }
+  // Lấy danh sách đơn hàng
+  getAllOrder(request?: any): Observable<any> {
+    let params = new HttpParams()
+    if (request) {
+      Object.keys(request).forEach(key => {
+        if (request[key] !== null && request[key] !== undefined) {
+          params = params.append(key, request[key])
+        }
+      })
+    }
+    // Gửi yêu cầu GET
+    return this.http.get(this.apiUrl, { params }).pipe(
+      catchError((error) => {
+        console.error('Error fetching orders:', error)
+        return throwError(() => new Error('Failed to fetch orders.'))
+      })
+    )
+  }
+}
